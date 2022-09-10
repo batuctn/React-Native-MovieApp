00610c8e03259bec4d9c388054e544f3db33faa0
batuctn <batuctnn14@gmail.com>
2022-09-10 04:06:36 +0300

    redux global state controll add

--- movieApp/src/Routes.js
+++ movieApp/src/Routes.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import {SafeAreaProvider} from 'react-native-safe-area-context';
+
 import AuthNavigaiton from './navigaiton/AuthNavigaiton';
 import MovieNavigaiton from './navigaiton/MovieNavigation';
+import {Provider, useSelector} from 'react-redux';
+import {store} from './store';
+
+const Contain = () => {
+  const userInRedux = useSelector(state => state.user.user);
+
+  return (
+    <NavigationContainer>
+      {userInRedux ? <MovieNavigaiton /> : <AuthNavigaiton />}
+    </NavigationContainer>
+  );
+};
 
 function Router() {
   return (
-    <SafeAreaProvider>
-      <NavigationContainer>
-        <AuthNavigaiton />
-        {/* <MovieNavigaiton /> */}
-        {/* {user ? <MovieNavigaiton /> : <AuthNavigaiton />} */}
-      </NavigationContainer>
-    </SafeAreaProvider>
+    <Provider store={store}>
+      <Contain />
+    </Provider>
   );
 }
 export default Router;
